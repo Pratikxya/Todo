5f3fddfaf67da89f1dfba2d9a028eb96ce83b8db
Pratikxya <pratikshyapoudel2000@gmail.com>
2022-11-11 18:14:04 +0545

    feat: :sparkles: Configured passport js

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ import helmet from 'helmet'
 
 import "./db/mongoose.js";
 import allRoutes from './routes/index.js'
-
+import configuredPassport from './utils/passport.js'
 
 const app = express();
 
 const PORT = process.env.port || 3000;
 
+// sasas(passport);
 
 app.use(json());
 app.use(helmet())
-
+app.use(configuredPassport.initialize())
 
 app.use(cors({ origin: '*' }));
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 import todosRouter from './todos.js'
+import authRouter from './users.js'
 import { Router } from 'express'
+import passport from 'passport'
 const router = Router()
 
-router.use('/todos',todosRouter)
+router.use('/todos', passport.authenticate('jwt', { session: false }), todosRouter)
+router.use('/auth', authRouter)
 
 export default router;
